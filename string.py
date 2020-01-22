08a6ae52bde7ee70ddeda2cade57ceff1d14ee88
root <root@instance-3.us-central1-a.c.ornate-crossbar-264305.internal>
2020-01-22 12:15:31 +0000

    variable string Practice

---
+++ string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#Variable synatax in python
+
+full_name = 'sai'
+
+mother_name = 'vasundara'
+
+info_mother_son = f'Mother name is {mother_name}, His son name is {full_name}.'
+print(info_mother_son.title())
